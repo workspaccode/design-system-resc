addd3452258ac9c742bccd91f29c49cd1fe07b3a
Mahmoud Hafez Eltarqi <workspaccode@gmail.com>
2026-06-16 11:52:40 +0300

    Split fields.dart and buttons-icon.dart into individual files per feature

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4048236948" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1740740333" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
